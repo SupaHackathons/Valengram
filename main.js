0f2092632471436f7ed2bd9e55cdd0092c1727c5
Nathan Hsieh <hsieh.nathan@gmail.com>
2016-02-13 11:15:41 -0800

    added react-router

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import Hello from './app/components/hello';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
 
 // This will render into the element with id 'react'
-ReactDOM.render(<Hello/>, document.getElementById('react'));
+render((
+  <Router history={hashHistory}>
+    <Route path='/' component={Hello} />
+  </Router>),
+  document.getElementById('react'));
